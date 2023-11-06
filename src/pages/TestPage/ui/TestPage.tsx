beb49f34dbed182da05940e5424b0e13ea56cc35
Yury Torgashin <imaginarylight@yandex.ru>
2023-11-06 15:23:50 +0300

    Feat/input (#21)
    * feat(input): refactor input component
    
    * feat(input): add validation
    
    * feat(input): create searching input, add yandex geosuggest

--- src/pages/TestPage/ui/TestPage.tsx
+++ src/pages/TestPage/ui/TestPage.tsx
@@ -1,25 +1,7 @@
-import React, { useState } from "react";
 import styles from "./TestPage.module.scss";
-import Input, { InputType } from "../../../components/ui/Input/Input.tsx";
 
 const TestPage = () => {
-  const [value, setValue] = useState("");
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-  };
-
-  return (
-    <div className={styles.testPage}>
-      <Input
-        label="Улица"
-        type={InputType.SEARCH}
-        placeholder="Поиск"
-        value={value}
-        onChange={handleChange}
-      />
-    </div>
-  );
+  return <div className={styles.testPage}>ТЕСТ</div>;
 };
 
 export default TestPage;
--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,1 +1,5 @@
 export const API_URL = import.meta.env.VITE_API_URL || "3001";
+export const GEOSUGGEST_KEY = import.meta.env.VITE_GEOSUGGEST_KEY;
+
+export const minAge = 3;
+export const maxAge = 16;
