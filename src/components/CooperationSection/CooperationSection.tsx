c18d968e4cb99dcea1483fe025d02140bce5bba0
Yuliya-Lov <jul-yaris@yandex.ru>
2023-11-06 14:26:11 +0300

    feat(reasonssection): create reason & reason section, add it in main page

--- src/components/CooperationSection/CooperationSection.tsx
+++ src/components/CooperationSection/CooperationSection.tsx
@@ -9,26 +9,30 @@ import cooperation from "../../assets/images/cooperation.png";
 const CooperationSection = () => {
   return (
     <section className={styles.cooperation}>
-      <HeadingIcon
-        headingLevel={headingLevel.H1}
-        title="Представителям спортшкол"
-        view={view.BLUE}
-      />
-      <div className={styles.twoColumns}>
-        <div className={styles.leftColumn}>
-          <h2 className={styles.headerQuestion}>Зачем с нами сотрудничать?</h2>
-          <p className={styles.description}>
-            Почему стоит зарегистрировать вашу спортшколу или секцию на нашей
-            платформе{" "}
-          </p>
-          <Accordeon accordionArr={accordionData.accordionArr} />
-        </div>
-        <ImageCard
-          className={styles.image}
-          src={cooperation}
-          alt="Дети с мячами"
-          size={ImageCardSize.EXTRA_LARGE}
+      <div className={styles.sectionContainer}>
+        <HeadingIcon
+          headingLevel={headingLevel.H2}
+          title="Представителям спортшкол"
+          view={view.BLUE}
         />
+        <div className={styles.twoColumns}>
+          <div className={styles.leftColumn}>
+            <h3 className={styles.headerQuestion}>
+              Зачем с нами сотрудничать?
+            </h3>
+            <p className={styles.description}>
+              Почему стоит зарегистрировать вашу спортшколу или секцию на нашей
+              платформе{" "}
+            </p>
+            <Accordeon accordionArr={accordionData.accordionArr} />
+          </div>
+          <ImageCard
+            className={styles.image}
+            src={cooperation}
+            alt="Дети с мячами"
+            size={ImageCardSize.EXTRA_LARGE}
+          />
+        </div>
       </div>
     </section>
   );
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,10 +1,13 @@
 import { FC } from "react";
 import styles from "./MainPage.module.scss";
+import ReasonsSection from "../../../components/ReasonsSection/ReasonsSection";
 import CooperationSection from "../../../components/CooperationSection/CooperationSection";
+import { reasonData } from "../../../utils/constants/reasonsData";
 
 const MainPage: FC = () => {
   return (
     <main className={styles.main}>
+      <ReasonsSection reasonArr={reasonData.reasonArr} />
       <CooperationSection />
     </main>
   );
