6efd93d4032b354bf345f8142823714c8b3cbaed
Melphi-S <imaginarylight@yandex.ru>
2023-11-18 18:43:19 +0300

    feat(searchpage): merge map

--- src/pages/SearchPage/ui/SearchPage.tsx
+++ src/pages/SearchPage/ui/SearchPage.tsx
@@ -3,13 +3,14 @@ import { useNavigate } from "react-router-dom";
 import CloseButton from "../../../components/ui/CloseButton/CloseButton.tsx";
 import styles from "./SearchPage.module.scss";
 import SearchHeader from "./SearchHeader/SearchHeader.tsx";
-import StepInitial from "./Step0/StepInitial.tsx";
+import StepInitial from "./StepInitial/StepInitial.tsx";
 import Button from "../../../components/ui/Button/Button.tsx";
 import { ButtonColor, ButtonTestId } from "../../../components/ui/Button/types";
 import Icon from "../../../components/ui/Icon/Icon.tsx";
 import { IconColor, IconTypes } from "../../../components/ui/Icon/types";
-import StepSports from "./Step1/StepSports.tsx";
+import StepSports from "./StepSports/StepSports.tsx";
 import StepAbout from "./StepAbout/StepAbout.tsx";
+import StepLocation from "./StepLocation/StepLocation.tsx";
 
 interface SearchPageProps extends HTMLAttributes<HTMLElement> {
   initialStep: number;
@@ -26,6 +27,7 @@ const SearchPage: FC<SearchPageProps> = (props) => {
     <StepInitial setStep={setStep} step={step} />,
     <StepSports setStep={setStep} step={step} />,
     <StepAbout setStep={setStep} step={step} />,
+    <StepLocation />,
   ];
 
   return (
--- src/pages/TestPage/ui/TestPage.tsx
+++ src/pages/TestPage/ui/TestPage.tsx
@@ -1,20 +1,4 @@
 import styles from "./TestPage.module.scss";
-// import YandexMap from "../../../components/Map/Map.tsx";
-//
-// const points = [
-//   {
-//     coordinates: [59.945168, 30.346637],
-//     content: "Северный орёл",
-//   },
-//   {
-//     coordinates: [59.939509, 30.367428],
-//     content: "Панчер",
-//   },
-//   {
-//     coordinates: [59.941094, 30.35818],
-//     content: "Медный всадник",
-//   },
-// ];
 
 const TestPage = () => {
   return <div className={styles.testPage}>ТЕСТ!!!</div>;
