8f37e3fb560a3b17a163baca9954953f11840f94
Yuliya-Lov <jul-yaris@yandex.ru>
2023-11-18 19:56:50 +0300

    feat(result card): create result card
    n

---
+++ src/components/ResultCard/types/index.ts
@@ -0,0 +1,25 @@
+export interface Adress {
+  index: number;
+  city: string;
+  metro: string;
+  district: string;
+  street: string;
+  house: string;
+}
+
+export interface SectionProps {
+  id: number;
+  sport_organization: string;
+  sport_type: string;
+  title: string;
+  address: Adress;
+  gender: string;
+  price: number;
+  raiting: number | null;
+  review_amount: number;
+  shedule: {
+    days: string;
+    time: string;
+  };
+  [key: string]: any;
+}
