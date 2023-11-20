a7ad8222c47b7bbaca7b12e7c17cebf798e9c669
Anna Smolskaya <anna31rus@mail.ru>
2023-11-20 19:19:58 +0500

    docs(db.json): add few items to db.json

--- src/components/ui/Preloader/Preloader.stories.tsx
+++ src/components/ui/Preloader/Preloader.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import Preloader from "./Preloader";
 import { PreloaderSize } from "./types";
 
@@ -8,36 +8,37 @@ export default {
   parameters: {
     docs: {
       description: {
-        component: 'Preloader используется для показа анимации загрузки. Вы можете выбрать один из трех размеров: маленький, средний и большой.'
-      }
-    }
-  }
+        component:
+          "Preloader используется для показа анимации загрузки. Вы можете выбрать один из трех размеров: маленький, средний и большой.",
+      },
+    },
+  },
 };
 
 export const Small = () => <Preloader size={PreloaderSize.Small} />;
 Small.parameters = {
   docs: {
-    storyDescription: 'Маленький Preloader размером 25px.'
-  }
+    storyDescription: "Маленький Preloader размером 25px.",
+  },
 };
 
 export const Medium = () => <Preloader size={PreloaderSize.Medium} />;
 Medium.parameters = {
   docs: {
-    storyDescription: 'Средний Preloader размером 50px.'
-  }
+    storyDescription: "Средний Preloader размером 50px.",
+  },
 };
 
 export const Large = () => <Preloader size={PreloaderSize.Large} />;
 Large.parameters = {
   docs: {
-    storyDescription: 'Большой Preloader размером 100px.'
-  }
+    storyDescription: "Большой Preloader размером 100px.",
+  },
 };
 
 export const DefaultPreloader = () => <Preloader />;
 DefaultPreloader.parameters = {
   docs: {
-    storyDescription: 'Preloader по умолчанию с размером 50px.'
-  }
+    storyDescription: "Preloader по умолчанию с размером 50px.",
+  },
 };
