7fb6c747c2d2b71f74e0314f9b8fd0411cd08713
Sergeev Dmitry <sy1er@yandex.ru>
2023-12-06 04:44:45 +0400

    feat(pagination): create pagination component add favotite btn to header

--- src/pages/FavouritePage/ui/FavouriteList/FavouriteList.tsx
+++ src/pages/FavouritePage/ui/FavouriteList/FavouriteList.tsx
@@ -1,36 +1,20 @@
 import { FC, useState } from "react";
 import styles from "./FavouriteList.module.scss";
 import ResultCard from "../../../ResultPage/ui/ResultCard/ResultCard";
-import Button from "../../../../components/ui/Button/Button";
-import {
-  ButtonColor,
-  ButtonTestId,
-} from "../../../../components/ui/Button/types";
 import { useFavourite } from "../../../../hooks/useLocalFavourites";
 import Badge from "../../../../components/ui/Badge/Badge";
 import { BadgeColor } from "../../../../components/ui/Badge/types";
 import Icon from "../../../../components/ui/Icon/Icon";
 import { IconColor, IconTypes } from "../../../../components/ui/Icon/types";
+import Pagination from "../../../../components/ui/Pagination/Pagination";
 
 const FavouriteList: FC = () => {
   const [favourite, setFavourite] = useFavourite();
-  const moreStep = 6;
-  const initialQuantity =
-    favourite.length >= moreStep ? moreStep : favourite.length;
-
-  const [quantity, setQuantity] = useState<number>(initialQuantity);
-
-  const showMore = () => {
-    if (favourite.length <= quantity + moreStep) {
-      setQuantity((pr) => pr + moreStep);
-    } else {
-      setQuantity(favourite.length);
-    }
-  };
+  const [quantity, setQuantity] = useState<number>(0);
 
   return (
-    <ul className={styles.cardList}>
-      <li className={styles.options}>
+    <section className={styles.listContainer}>
+      <div className={styles.options}>
         <p className={styles.description}>Сохранено {favourite.length}</p>
         {favourite.length > 0 && (
           <Badge
@@ -42,37 +26,29 @@ const FavouriteList: FC = () => {
             <Icon type={IconTypes.CROSS} color={IconColor.SECONDARY} />
           </Badge>
         )}
-      </li>
-      {favourite &&
-        favourite.slice(0, quantity).map((section) => {
-          return (
-            <ResultCard
-              key={section.id}
-              section={section}
-              favourite={favourite}
-              setFavourite={setFavourite}
-            />
-          );
-        })}
-      <li className={styles.more}>
+      </div>
+      <ul className={styles.cardList}>
+        {favourite &&
+          favourite.slice(0, quantity).map((section) => {
+            return (
+              <li key={section.id}>
+                <ResultCard
+                  section={section}
+                  favourite={favourite}
+                  setFavourite={setFavourite}
+                />
+              </li>
+            );
+          })}
         {quantity < favourite.length && (
-          <>
-            <Button
-              testId={ButtonTestId.FORWARD}
-              color={ButtonColor.PRIMARY}
-              withMinWidth
-              type="button"
-              onClick={showMore}
-            >
-              Загрузить еще
-            </Button>
-            <span className={styles.moreCount}>
-              {quantity} из {favourite.length}
-            </span>
-          </>
+          <Pagination
+            setValue={setQuantity}
+            value={quantity}
+            list={favourite}
+          />
         )}
-      </li>
-    </ul>
+      </ul>
+    </section>
   );
 };
 
