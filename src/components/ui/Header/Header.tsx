bc48361c6d6fb5dbad4ce340e27c512e49da6975
Anna Smolskaya <anna31rus@mail.ru>
2023-12-11 00:19:16 +0500

    feat(mainpage adaptive): adaptive for Header, AboutSection, ReasonSection

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -44,6 +44,13 @@ const Header: FC = () => {
               <Icon type={IconTypes.RIGHT_ICON} />
             </>
           </Button>
+          <button
+            type="button"
+            className={styles.profileButton}
+            onClick={() => navigate("/signin")}
+          >
+            <span className={styles.hiddenText}>Войти</span>
+          </button>
         </div>
       </div>
     </header>
